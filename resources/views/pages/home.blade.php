797b3cdd4cb7a254d3aac84f0d8278106a3f7b77
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-11-29 13:51:03 +1300

    Create HomeController, Add assets, add home page view, add Post model

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Dashr</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400|Roboto:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/article-nice.css">
+    <link rel="stylesheet" href="assets/css/Articles-Cards-images.css">
+    <link rel="stylesheet" href="assets/css/Black-Navbar.css">
+    <link rel="stylesheet" href="assets/css/Color-Contact-List.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Basic-icons.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="background: #f3f2f3;">
+    <nav class="navbar navbar-dark navbar-expand-md bg-dark py-3">
+        <div class="container"><a class="navbar-brand d-flex align-items-center" href="/"><span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-bezier">
+                        <path fill-rule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z"></path>
+                        <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z"></path>
+                    </svg></span><span>Dashr</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
+                    <li class="nav-item dropdown"><a class="dropdown-toggle nav-link" aria-expanded="false" data-bs-toggle="dropdown" href="#">Categories</a>
+                        <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html">Contact Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Random Article</a></li>
+                </ul><a class="btn btn-primary ms-md-2" role="button" href="login.html" style="margin-right: 10px;">Log In</a><a class="btn btn-primary ms-md-2" role="button" href="register.html" style="background: rgb(255,255,255);color: rgb(0,0,0);">Sign Up</a>
+            </div>
+        </div>
+    </nav>
+    <section>
+        <div class="container" style="background: #ffffff;margin-top: 10px;margin-bottom: 10px;">
+            <div class="row mb-2" style="padding-bottom: 10px;border-width: 3px;border-color: rgb(29,119,208);padding-top: 10px;">
+                <div class="col-md-12">
+                    <div class="d-xl-flex align-items-xl-center"><a href="/lorem.html"><img class="img-fluid" src="assets/img/test_image.jpg" style="height: 170px;" loading="lazy"></a>
+                        <div style="padding-left: 10px;">
+                            <h3 class="d-xl-flex">Cras justo odio, dapibus ac facilisis in, egestas eget<br></h3>
+                            <p class="d-xl-flex">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio,<br></p>
+                            <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                                <div>
+                                    <p class="fw-bold mb-0">John Smith</p>
+                                    <p class="text-muted mb-0">Erat netus</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container" style="background: #ffffff;margin-top: 10px;margin-bottom: 10px;">
+            <div class="row mb-2" style="padding-bottom: 10px;border-width: 3px;border-color: rgb(29,119,208);padding-top: 10px;">
+                <div class="col-md-12">
+                    <div class="d-xl-flex align-items-xl-center"><a href="/lorem.html"><img class="img-fluid" src="assets/img/test_image.jpg" style="height: 170px;" loading="lazy"></a>
+                        <div style="padding-left: 10px;">
+                            <h3 class="d-xl-flex">Cras justo odio, dapibus ac facilisis in, egestas eget<br></h3>
+                            <p class="d-xl-flex">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio,<br></p>
+                            <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                                <div>
+                                    <p class="fw-bold mb-0">John Smith</p>
+                                    <p class="text-muted mb-0">Erat netus</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container" style="background: #ffffff;margin-top: 10px;margin-bottom: 10px;">
+            <div class="row mb-2" style="padding-bottom: 10px;border-width: 3px;border-color: rgb(29,119,208);padding-top: 10px;">
+                <div class="col-md-12">
+                    <div class="d-xl-flex align-items-xl-center"><a href="/lorem.html"><img class="img-fluid" src="assets/img/test_image.jpg" style="height: 170px;" loading="lazy"></a>
+                        <div style="padding-left: 10px;">
+                            <h3 class="d-xl-flex">Cras justo odio, dapibus ac facilisis in, egestas eget<br></h3>
+                            <p class="d-xl-flex">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio,<br></p>
+                            <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                                <div>
+                                    <p class="fw-bold mb-0">John Smith</p>
+                                    <p class="text-muted mb-0">Erat netus</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container" style="background: #ffffff;margin-top: 10px;margin-bottom: 10px;">
+            <div class="row mb-2" style="padding-bottom: 10px;border-width: 3px;border-color: rgb(29,119,208);padding-top: 10px;">
+                <div class="col-md-12">
+                    <div class="d-xl-flex align-items-xl-center"><a href="/lorem.html"><img class="img-fluid" src="assets/img/test_image.jpg" style="height: 170px;" loading="lazy"></a>
+                        <div style="padding-left: 10px;">
+                            <h3 class="d-xl-flex">Cras justo odio, dapibus ac facilisis in, egestas eget<br></h3>
+                            <p class="d-xl-flex">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio,<br></p>
+                            <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                                <div>
+                                    <p class="fw-bold mb-0">John Smith</p>
+                                    <p class="text-muted mb-0">Erat netus</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container" style="background: #ffffff;margin-top: 10px;margin-bottom: 10px;">
+            <div class="row mb-2" style="padding-bottom: 10px;border-width: 3px;border-color: rgb(29,119,208);padding-top: 10px;">
+                <div class="col-md-12">
+                    <div class="d-xl-flex align-items-xl-center"><a href="/lorem.html"><img class="img-fluid" src="assets/img/test_image.jpg" style="height: 170px;" loading="lazy"></a>
+                        <div style="padding-left: 10px;">
+                            <h3 class="d-xl-flex">Cras justo odio, dapibus ac facilisis in, egestas eget<br></h3>
+                            <p class="d-xl-flex">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio,<br></p>
+                            <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                                <div>
+                                    <p class="fw-bold mb-0">John Smith</p>
+                                    <p class="text-muted mb-0">Erat netus</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container" style="background: #ffffff;margin-top: 10px;margin-bottom: 10px;">
+            <div class="row mb-2" style="padding-bottom: 10px;border-width: 3px;border-color: rgb(29,119,208);padding-top: 10px;">
+                <div class="col-md-12">
+                    <div class="d-xl-flex align-items-xl-center"><a href="/lorem.html"><img class="img-fluid" src="assets/img/test_image.jpg" style="height: 170px;" loading="lazy"></a>
+                        <div style="padding-left: 10px;">
+                            <h3 class="d-xl-flex">Cras justo odio, dapibus ac facilisis in, egestas eget<br></h3>
+                            <p class="d-xl-flex">Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio,<br></p>
+                            <div class="d-flex"><img class="rounded-circle flex-shrink-0 me-3 fit-cover" width="50" height="50" src="https://cdn.bootstrapstudio.io/placeholders/1400x800.png">
+                                <div>
+                                    <p class="fw-bold mb-0">John Smith</p>
+                                    <p class="text-muted mb-0">Erat netus</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="container d-xl-flex justify-content-xl-center">
+        <div class="row" style="padding: 2px;">
+            <div class="col">
+                <nav>
+                    <ul class="pagination">
+                        <li class="page-item"><a class="page-link" aria-label="Previous" href="#"><span aria-hidden="true">«</span></a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">4</a></li>
+                        <li class="page-item"><a class="page-link" href="#">5</a></li>
+                        <li class="page-item"><a class="page-link" aria-label="Next" href="#"><span aria-hidden="true">»</span></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <footer class="text-center text-white bg-dark">
+        <div class="container p-4 pb-0">
+            <section class="mb-4"><a class="btn btn-outline-light btn-floating m-1" role="button" href="#!"><i class="fab fa-facebook-f"></i></a><a class="btn btn-outline-light btn-floating m-1" role="button" href="#!"><i class="fab fa-twitter"></i></a><a class="btn btn-outline-light btn-floating m-1" role="button" href="#!"><i class="fab fa-google"></i></a><a class="btn btn-outline-light btn-floating m-1" role="button" href="#!"><i class="fab fa-instagram"></i></a><a class="btn btn-outline-light btn-floating m-1" role="button" href="#!"><i class="fab fa-linkedin-in"></i></a><a class="btn btn-outline-light btn-floating m-1" role="button" href="#!"><i class="fab fa-github"></i></a></section>
+        </div>
+        <div class="text-center p-3" style="background-color:rgba(0, 0, 0, 0.2);"><span> © 2022 Copyright: </span><a class="text-white" href="https://github.com/">Dashr</a></div>
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/zectStudio---Sidebar-Menu-by-bbbootstrap.js"></script>
+</body>
+
+</html>
