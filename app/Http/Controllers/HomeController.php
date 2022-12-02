616f7005ef0639a680e7ccb9e81587989be366b5
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-12-02 13:32:04 +1300

    add category single page with search query filter #27

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -17,7 +18,7 @@ class HomeController extends Controller
     {
         return view('pages.home', [
             'title' => 'Dashr | Home Page',
-            'posts' => Post::latest()->paginate(8),
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(8)->withQueryString()
         ]);
     }
 }
