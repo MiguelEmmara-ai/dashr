f93d9b62121459502ad2bc913f643f05e2bcb7c3
MiguelEmmara-ai <m.miguelemmara@gmail.com>
2022-12-08 00:17:17 +1300

    minor fix user status on navbar

--- resources/views/includes/admin/navbar.blade.php
+++ resources/views/includes/admin/navbar.blade.php
@@ -60,7 +60,7 @@
                                             </div>
                                             <div class="flex-grow-1">
                                                 <span class="fw-semibold d-block">{{ $user->name }}</span>
-                                                <small class="text-muted">Admin</small>
+                                                <small class="text-muted">{{ $user->user_types }}</small>
                                             </div>
                                         </div>
                                     </a>
